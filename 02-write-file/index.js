461919ee2fb3f6c00dba4509093c79bb1f0ce3b6
dusixx <113414935+dusixx@users.noreply.github.com>
2025-01-20 22:50:22 +0200

    fix: update 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,22 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const os = require('os');
+const { EOL } = require('os');
 
-const { EOL } = os;
 const TARGET_FILENAME = 'input.txt';
 const FAREWALL_PHRASE = 'Have a good one!\n';
 
 const { stdin: input, stdout: output } = process;
 const rl = readline.createInterface({ input, output });
-
 const ws = fs.createWriteStream(path.join(__dirname, TARGET_FILENAME));
 
-console.log(
-  `\x1B[2J\nAll input will be saved to a file "${TARGET_FILENAME}"\nType "exit" or press Ctrl+C to finish\n`,
-);
+console.log(`\x1B[2JAll input will be saved to a file "${TARGET_FILENAME}`);
+console.log('Type "exit" or press Ctrl+C to finish\n');
 
 process.on('exit', () => {
+  ws.end();
   process.stdout.write(`\n${FAREWALL_PHRASE}`);
 });
 
@@ -24,7 +22,7 @@ rl.setPrompt('> ');
 rl.prompt();
 
 rl.on('line', (line) => {
-  if (/\s*exit\s*/.test(line)) {
+  if (/^\s*exit\s*$/.test(line)) {
     rl.close();
   }
   ws.write(`${line}${EOL}`);
